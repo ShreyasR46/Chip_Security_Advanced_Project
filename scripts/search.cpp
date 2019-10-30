7252b4535fa92ae588e04a9b493e9b2b3a3f3f42
kartikey purohit <kp2377@nyu.edu>
2019-10-30 17:04:34 -0400

    search completely workingls -l

--- scripts/search.cpp
+++ scripts/search.cpp
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<iostream>
-#include <bits/stdc++.h>
 #include<string>
 
 #define AUTO
@@ -13,11 +12,111 @@ using namespace std;
 
 };*/
 
+
+class Node  
+{  
+    public: 
+    string data;  
+    Node *next;  
+};  
+
+
+void printList(Node* n) 
+{ 
+    while (n != NULL) 
+    { 
+        cout << n->data << " "; 
+        n = n->next; 
+    } 
+} 
+
+void deleteList(Node** head_ref)  
+{  
+      
+/* deref head_ref to get the real head */
+Node* current = *head_ref;  
+Node* next;  
+  
+while (current != NULL)  
+{  
+    next = current->next;  
+    free(current);  
+    current = next;  
+}  
+      
+/* deref head_ref to affect the real head back  
+    in the caller. */
+*head_ref = NULL;  
+}
+
+/*void append(Node*& reference, string str_data) 
+{ 
+   Node* new_node = new Node();
+   //Node* last_node = new Node();
+   new_node->data = str_data;
+   if(*reference->next != NULL)
+   { 
+	//new_node = reference->next;
+	append(&(reference->next), str_data);
+   }
+   else
+   {
+       	reference->next = new_node;
+   }	
+   return;  
+}*/
+
+
+void append(Node*& p, string d){
+    Node* temp = new Node();
+    temp->data=d;
+    if(p == NULL) p = temp;
+    else append(p->next, d);
+}
+
+
+
 int main()
 {
 	string path_to_search, keyword, filename_to_save;
+	char dot_sv = ':';
+        clock_t start, end;
+  	double cpu_time_used;
+	FILE *dictionary, *filename, *data;
+  	long int n =0;
+  	long int total_lines=0, flag = 0,i = 0, k =0;
+  	char ct = '`';
+ 	char *line = NULL;
+  	size_t len = 0;
+  	long int offset = 2;
+  	int counter=0;
+        string search = "banana";
+        string copied;
+	long int alphabet_array[27];
+        long int temp=0, nread;
+
+
+	//Initializing linked List
+  Node* head = NULL;
+  Node* second = NULL;
+  Node* base = NULL;
+  ///
+  head = new Node();
+  second = new Node();
+  base = new Node();
+  //connecting head and base
+  head->data = "Kartikey";
+  head->next = second;
+  
+  second->data = "Ashish";
+  second->next = base;
+  
+  base->data= "Deepraj";
+  base->next = NULL;
+
+////////////////////////////////////////////////////////////////
 #ifdef AUTO
-	path_to_search = "/home/parallels/Documents/Chip_Security_Advanced_Project/ariane/";
+	path_to_search = "~/Documents/Github_Directories/Chip_Security_Advanced_Project/ariane/";
 	keyword = "\"secure_reg\"";
 	filename_to_save = "search_results.txt";
 #else
@@ -37,5 +136,59 @@ int main()
 	const char *command = command1.c_str();
 	cout << "Compiling file using " << command << endl; 
    	system(command);
+
+/////////////////////////////////////////////////////////////////
+	
+	filename = fopen("filename.txt","w");
+	data = fopen("data.txt","w");
+	dictionary = fopen("search_results.txt", "r");
+  	if(dictionary == NULL)
+  	{
+		perror ("Error opening file.");
+ 	}
+	else
+	{
+		while((nread= getline(&line,&len,dictionary))!=-1)
+		{
+			cout<<"The nread value is: "<<nread<<"\n";
+			printf("length = %ld: \n total length = %ld\n\n\n", nread, temp);
+			for(int i=0, flag = 0;i<nread;i++)
+			{		
+				if(flag == 1)
+				{	
+					putc(line[i], data);
+					cout<<line[i];
+				}
+				if(dot_sv == line[i] && (flag == 0))
+				{
+					flag = 1;
+					putc('\n', filename);
+					putc('\r', filename);
+					cout<<"\n\r";
+				}
+				if(flag == 0)
+				{
+					putc(line[i], filename);
+					cout<<line[i];
+				}
+				
+				//if(j == 2){j = 0;}
+			}
+			//putc('\n', data);
+			//putc('\r', data);
+			cout<<"\n\r";
+		}
+	}
+  	free(line); 
+  	fclose(dictionary);
+	fclose(filename);
+	fclose(data);
+
+  	end = clock();
+  	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+ 	cout<<"Second is : "<<cpu_time_used<<"\n";
+  
+
+	
 	return 0;
 }
