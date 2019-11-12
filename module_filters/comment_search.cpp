86282c1d57dcbfb723ae6d9922008588c4c38f10
ags586 <44849617+ags586@users.noreply.github.com>
2019-11-11 20:45:28 -0500

    Comment Search

---
+++ module_filters/comment_search.cpp
@@ -0,0 +1,32 @@
+/*
+ * comment_search.cpp
+ *
+ *  Created on: Nov 11, 2019
+ *      Author: ags58
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1;
+	cout << "Enter the string:" << endl;
+	getline(cin, str1);
+	int i,j;
+	for(i=0,j=1;i<str1.length(),j<str1.length();i++,j++)
+	{
+		if(str1[i]=='/' && str1[j]== '/')
+		{
+			cout << "EUREKA" << endl;
+			break;
+		}
+		else cout << "Comments not found" << endl;;
+
+	}
+	return 0;
+}
+
+
